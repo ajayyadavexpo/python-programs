5ee741c74a5c918d5f9cfad6d0240076f84d427f
AjayYadavAi <proajayyadav0525@gmail.com>
2020-02-03 21:24:44 +0530

    click

--- link-click.py
+++ link-click.py
@@ -3,9 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome('C:/xampp/htdocs/web/python-programs/chromedriver.exe')  # Optional argument, if not specified will search path.
-driver.get('https://www.pexels.com/search/free%20download/');
+driver.get('https://www.guru99.com/selenium-python.html');
 time.sleep(4) # Let the user actually see something!
-search_box = driver.find_elements_by_css_selector('js-photo-link')
+search_box = driver.find_element_by_link_text('Python')
 search_box.click()
 
 time.sleep(5) # Let the user actually see something!
