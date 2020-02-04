19c4a3535120641e1ffd9db8c70e7300d9935795
AjayYadavAi <proajayyadav0525@gmail.com>
2020-02-04 21:00:25 +0530

    update

--- image-download.py
+++ image-download.py
@@ -1,12 +1,34 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+path = 'C:/xampp/htdocs/web/python-programs/chromedriver.exe'
 
-driver = webdriver.Chrome('C:/xampp/htdocs/web/python-programs/chromedriver.exe')  # Optional argument, if not specified will search path.
-driver.get('https://www.pexels.com/search/free%20download/');
+
+chrome_options = webdriver.ChromeOptions()
+prefs = {"profile.default_content_setting_values.notifications" : 1}
+chrome_options.add_experimental_option("prefs",prefs)
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+
+
+url = "https://unsplash.com/"
+
+driver.get(url);
 time.sleep(4) # Let the user actually see something!
-search_box = driver.find_elements_by_css_selector('js-photo-link')
-search_box.click()
+search_box = driver.find_element_by_name("searchKeyword")
+search_box.send_keys("Technology")
+button = driver.find_element_by_class_name('_2VoZY')
+button.click()
+time.sleep(2)
+image = driver.find_element_by_class_name('_2zEKz')
+image.click()
+time.sleep(2)
+
+
+download_link = driver.find_element_by_class_name('_13Q-')
+download_link.click()
+
 
 time.sleep(5) # Let the user actually see something!
 driver.quit()
