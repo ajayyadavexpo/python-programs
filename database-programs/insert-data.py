d7f6739fa54c3ae8884fdd3b51f2ef7e9c06210b
AjayYadavAi <proajayyadav0525@gmail.com>
2020-02-06 21:05:47 +0530

    'insert-data'

---
+++ database-programs/insert-data.py
@@ -0,0 +1,22 @@
+import mysql.connector  
+#Create the connection object   
+myconn = mysql.connector.connect(host = "localhost", user = "root",passwd = "",database = "pythondb")  
+#creating the cursor object  
+cur = myconn.cursor()  
+sql = "insert into Employee(name, id, salary, dept_id) values (%s, %s, %s, %s)"  
+  
+#The row values are provided in the form of tuple   
+val = ("John", 110, 25000.00, 201)  
+  
+try:  
+    #inserting the values into the table  
+    cur.execute(sql,val)  
+  
+    #commit the transaction   
+    myconn.commit()  
+      
+except:  
+    myconn.rollback()  
+  
+print(cur.rowcount,"record inserted!")  
+myconn.close()  
