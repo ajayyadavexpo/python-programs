93deeb1d1fed8fe5d116250cf8c78c82bbf1dcb7
AjayYadavAi <proajayyadav0525@gmail.com>
2020-02-29 09:12:45 +0530

    calander

---
+++ calander_print.py
@@ -0,0 +1,12 @@
+# importing calendar library 
+import calendar 
+  
+def printcalendar(year): 
+       
+     # printing calendar 
+     print(calendar.calendar(2019)) 
+  
+# driver program to test above function 
+
+year = 2019
+printcalendar(year) 
