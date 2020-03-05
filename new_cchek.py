c7233eb4741c622f01cb1d8385f20ec97d7393d6
AjayYadavAi <proajayyadav0525@gmail.com>
2020-03-05 19:56:27 +0530

    added

---
+++ new_cchek.py
@@ -0,0 +1,11 @@
+# List initialization 
+list_string = ['21', '1', '131', '12', '15'] 
+  
+# mapping 
+list_map = map(int, list_string) 
+  
+# sorting list 
+list_sorted = sorted(list_map) 
+  
+# Printing sorted list of integers 
+print(list_sorted) 
